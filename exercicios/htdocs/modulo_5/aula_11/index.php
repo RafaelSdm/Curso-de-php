fe10d46efa78ee10f466f73a52d7ce61b96338c9
RafaelSdm <RafaelSdm@users.noreply.github.com>
2021-01-05 18:11:39 -0300

    Create index.php

---
+++ exercicios/htdocs/modulo_5/aula_11/index.php
@@ -0,0 +1,18 @@
+<?php
+
+	$n1 = 10;
+	$n2 = 6;
+	$n3 = 53;
+
+	if ($n1 >$n2 && $n1 > $n3) {
+		echo "o maior numero é o $n1";
+
+		# code...
+	}elseif ($n2 > $n1 && $n2> $n3) {
+		echo "o maior numero é o $n2";
+		# code...
+	}else{
+		echo "o maior numero é o $n3";
+	}
+
+?>
