da3dfd079b65f58ea1198e1c76ddd3a952e040ca
RafaelSdm <RafaelSdm@users.noreply.github.com>
2021-01-05 18:33:00 -0300

    Create index.php

---
+++ exercicios/htdocs/modulo_5/aula_12/index.php
@@ -0,0 +1,27 @@
+<?php
+	
+	 for ($i=0; $i <10 ; $i++) { 
+	 	echo $i +1;
+	 	echo "<hr>";
+	 }
+
+	 $i = 0;
+
+	 while ($i <= 10) {
+	 	echo "hello world";
+	 	echo "<br/>";
+	 	$i++;
+	 	# code...
+	 }
+
+	 $i = 100;
+
+	 do {
+	 	echo "$i";
+	 	echo "<br/>";
+	 	# code...
+	 	$i--;
+
+	 } while ($i >= 1);
+
+?>
