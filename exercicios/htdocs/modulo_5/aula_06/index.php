9aed16085853ddc8108df652e1926fe7354bc660
RafaelSdm <RafaelSdm@users.noreply.github.com>
2021-01-05 16:13:47 -0300

    Create index.php

---
+++ exercicios/htdocs/modulo_5/aula_06/index.php
@@ -0,0 +1,32 @@
+<?php
+	
+	$nome =  array('Rafael','Ambrosio','Tonho');
+
+	echo $nome[1];
+	echo "<br/>";
+
+
+	$var = ['Joao','Rick','Mario'];
+
+	echo $var[0];
+	echo "<br/>";
+
+	$vetor = ['Doug',21,true,10.89];
+
+	echo $vetor[2];
+	echo "<br/>";
+
+	$informacaao['nome'] = "Rafael";
+	$informacaao['idade'] = 19;
+	$informacaao['cidade'] = 'Capela Nova';
+
+	echo $informacaao['nome'];
+	echo "<br/>";
+	echo $informacaao["idade"];
+	echo "<br/>";
+	echo $informacaao['cidade'];
+	echo "<br/>";
+
+
+
+?>
