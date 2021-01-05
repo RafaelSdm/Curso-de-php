e5d84eed5f3f79cffd322cd0a6dd30e2c90abd98
RafaelSdm <RafaelSdm@users.noreply.github.com>
2021-01-05 14:21:52 -0300

    commit full course

---
+++ exercicios/htdocs/modulo_5/aula_01/index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Minha pagina de teste</title>
+	<meta charset="utf-8">
+</head>
+<body>
+
+	<p>
+		<?php
+			 echo "Hello world!";
+
+		?>
+		
+
+	</p>
+
+</body>
+</html>
