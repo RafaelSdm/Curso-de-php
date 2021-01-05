17b039531b19b5f867c315d202e1227cc081a0c6
RafaelSdm <RafaelSdm@users.noreply.github.com>
2021-01-05 17:42:02 -0300

    commit full course

---
+++ exercicios/htdocs/modulo_5/aula_09/index.php
@@ -0,0 +1,13 @@
+<?php
+
+	$var1 = "Rafael";
+	$var2 = "dd";
+
+	if ($var2 == $var1) {
+		echo '<div style="width:300px";height:300px;background:red;"></div>';
+		
+	}else{
+		echo "Os nomes são totalmentes diferentes";
+	}
+
+?>
---
+++ exercicios/htdocs/modulo_5/aula_09/index_1.php
@@ -0,0 +1,17 @@
+<?php
+
+		$n1 = 5 + 23;
+		$n2 = '23+5';
+
+		if ($n2==$n1) {
+			echo "Afiração verdadeira";
+			# code...
+		}else if ($n1 != $n2) {
+			echo "Afirmação de diferença";
+			# code...
+		}else{
+			echo "invalido";
+		}
+
+
+?>
