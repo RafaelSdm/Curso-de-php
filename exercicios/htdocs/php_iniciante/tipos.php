e1f423239054016a10b95d798ef36fa0083d9839
RafaelSdm <RafaelSdm@users.noreply.github.com>
2021-01-06 15:22:39 -0300

    Create tipos.php

---
+++ exercicios/htdocs/php_iniciante/tipos.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title></title>
+</head>
+<body>
+	<?php 
+
+		$numero = 12;
+		$nome = "Rafael";
+		echo $numero;
+		echo "<br/>";
+		echo $nome;
+		echo "<br/>";
+		echo "$nome tem $numero anos";
+
+	 ?>
+
+</body>
+</html>
