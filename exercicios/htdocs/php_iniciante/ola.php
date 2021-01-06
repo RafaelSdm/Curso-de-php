17db8d74cb88617f258c3d1d5327c3f171fee8d1
RafaelSdm <RafaelSdm@users.noreply.github.com>
2021-01-06 15:09:42 -0300

    Create ola.php

---
+++ exercicios/htdocs/php_iniciante/ola.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>Primeiro exemplo de php</title>
+	<style type="text/css">
+		h2{
+			color: blue;
+			text-shadow: 1px 1px 1px black;
+		}
+	</style>
+</head>
+<body>
+	<h1>Testando php</h1>
+	<?php
+	echo "<h2>ola mundo!</h2>";
+
+	?>
+
+</body>
+</html>
