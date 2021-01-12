efe01b41c24f0268080794d1eaee0b50c7caab27
RafaelSdm <rafaeldamasceno2000@gmail.com>
2021-01-12 13:24:59 -0300

    Create 01for.php

---
+++ exercicios/htdocs/php_iniciante/01for.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="pt - br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+
+        $contador = 1;
+        for($i=0;$i<=10;$i++){
+            echo " $i ";
+          
+        }
+        echo "</br>";
+        echo "<hr>";
+
+        for($i=10;$i>=0;$i--){
+            echo " $i ";
+        }
+
+    ?>
+    
+</body>
+</html>
